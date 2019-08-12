4bcb387cadd5eaf9de9dad88e5fa41d817c964e1
Mayank Jindal <jindalit@gmail.com>
2019-08-12 09:53:09 +0530

    checkout address save

--- src/app/checkout/checkout-page/checkout-page.component.ts
+++ src/app/checkout/checkout-page/checkout-page.component.ts
@@ -7,7 +7,7 @@ import{DataService} from '../../data/data.service';
 })
 export class CheckoutPageComponent implements OnInit {
   inputModel: any;
-
+  address:any =[];
   radioModel: any;
 
   selectModel: any;
@@ -16,5 +16,17 @@ export class CheckoutPageComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.data.fetchData('/mp/cart').subscribe(data => {
+      let apiData = (data as any);
+      this.data.bagProduct = JSON.parse(apiData._body).data.items;
+
+      this.data.bagProduct.map((item) => {
+        this.data.total = this.data.total + (parseInt(item.itemPrice) * item.itemQuantity);
+        this.data.cartCount = this.data.cartCount + item.itemQuantity;
+      });
+    })
+    this.data.fetchData('/mp/shippingAddress').subscribe(data => {
+      this.address = JSON.parse((data as any)._body).data;
+    })
   }
 }
